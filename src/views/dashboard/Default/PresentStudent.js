f7f7d567fa54f438044abd5455869245c794e33f
abhijbs01 <abhishek.jbs01@gmail.com>
2024-05-13 15:49:44 +0530

    attandance

--- src/views/dashboard/Default/PresentStudent.js
+++ src/views/dashboard/Default/PresentStudent.js
@@ -4,14 +4,12 @@ import MainCard from "ui-component/cards/MainCard";
 import { useEffect, useState } from "react";
 import { useGetAttendanceLogs } from "hooks/useGetAttendanceLogs";
 import PASStudent from "../../../assets/images/icone deshbord/vector6.png";
-import { useGetSingleStudent } from "hooks/useGetSingleStudent";
 import { useGetAllStudents } from "hooks/useGetAllStudents";
 
 const CardWrapper = styled(MainCard)(() => ({
   backgroundColor: "#fff",
   color: "#1B1D28",
-  boxShadow:
-    "rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;",
+  boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;",
 }));
 
 const PresentStudent = () => {
@@ -20,40 +18,31 @@ const PresentStudent = () => {
   const [totalPresent, setTotalPresent] = useState(0);
   const [totalAbsent, setTotalAbsent] = useState(0);
   const { data, refetch } = useGetAttendanceLogs(selectedDate, selectedType);
-  const { data:student } = useGetAllStudents();
-  const TotalStudent=student?.totalStudents;
+  const { data: allStudents } = useGetAllStudents();
+  const TotalStudent = allStudents?.totalStudents;
 
   useEffect(() => {
     refetch();
   }, [selectedDate, selectedType, refetch]);
-  
+
   useEffect(() => {
     if (data && Array.isArray(data)) {
       const filterStudents = data.filter(
-        (entry, index, arr) =>
-          arr.findIndex((e) => e.studentId === entry.studentId) === index
+        (entry, index, arr) => arr.findIndex((e) => e.studentId === entry.studentId) === index
       );
-      const presentCount = filterStudents.filter(
-        (entry) => entry.status === "Present"
-      ).length;
-  
-      const absentCount = TotalStudent-presentCount
+      const presentCount = filterStudents.filter((entry) => entry.status === "Present").length;
+
+      const absentCount = TotalStudent - presentCount;
 
       setTotalPresent(presentCount);
       setTotalAbsent(absentCount);
     }
-  }, [data]);
-  
+  }, [data, TotalStudent]);
 
   return (
     <CardWrapper border={true} content={false}>
       <Box sx={{ p: 2.25 }}>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-around"
-          alignItems="center"
-        >
+        <Grid container direction="row" justifyContent="space-around" alignItems="center">
           <Grid item>
             <img
               src={PASStudent}
@@ -68,22 +57,11 @@ const PresentStudent = () => {
           </Grid>
           <Grid item>
             <Grid container direction="column">
-              <Typography
-                sx={{
-                  fontSize: "14px",
-                  fontWeight: 500,
-                  color: "#1B1D28",
-                }}
-              >
+              <Typography sx={{ fontSize: "14px", fontWeight: 500, color: "#1B1D28" }}>
                 P/A Student
               </Typography>
               <Typography
-                sx={{
-                  fontSize: "26px",
-                  fontWeight: 500,
-                  textAlign: "center",
-                  color: "#1B1D28",
-                }}
+                sx={{ fontSize: "26px", fontWeight: 500, textAlign: "center", color: "#1B1D28" }}
               >
                 {totalPresent} / {totalAbsent}
               </Typography>
